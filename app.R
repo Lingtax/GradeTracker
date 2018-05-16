4a505fbdd7135111dbe619f5c09ce12cb2b4c3bd
Mathew Ling <m.ling@deakin.edu.au>
2018-05-16 21:23:52 +1000

    Version 0.1

--- app.R
+++ app.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(ggplot2)
 
 ui <- navbarPage(
   "HPS111 Grade Tracker",
@@ -35,13 +36,13 @@ ui <- navbarPage(
                  max = 12,
                  value = 0
                ),
-               numericInput(
-                 "exam",
-                 "Exam (%):",
-                 min = 0,
-                 max = 12,
-                 value = 0
-               ),
+               # numericInput(
+               #   "exam",
+               #   "Exam (%):",
+               #   min = 0,
+               #   max = 12,
+               #   value = 0
+               #),
                
                
                hr(),
@@ -66,9 +67,8 @@ ui <- navbarPage(
              
              # Show a plot of the generated distribution
              mainPanel(
-               #uiOutput("weights"),
-               #hr(),
-               textOutput("statement"),
+               textOutput("statement1"),
+               textOutput("statement2"),
                plotOutput("targetPlot")
              )
            )),
@@ -101,22 +101,22 @@ server <- function(input, output) {
   
   # If NA treat as 0 
   # Count latest non-0 response as time-location in trimester.
-  week <- reactive({ifelse(input$exam>0, 13, 
-                           ifelse(input$at2>0, 12, 
-                                  ifelse(input$quiz3>0, 11,
-                                         ifelse(input$at1>0, 9,
-                                                ifelse(input$quiz2>0, 7,
-                                                       ifelse(input$quiz1>0, 4,0
-                                                       ))))))
+  week <- reactive({#ifelse(input$exam>0, 6, 
+                           ifelse(input$at2>0, 6, 
+                                  ifelse(input$quiz3>0, 5,
+                                         ifelse(input$at1>0, 4,
+                                                ifelse(input$quiz2>0, 3,
+                                                       ifelse(input$quiz1>0, 2, 1
+                                                       )))))#)
   })
   
-  denom <- reactive({ifelse(input$exam>0, 100, 
+  denom <- reactive({#ifelse(input$exam>0, 100, 
                             ifelse(input$at2>0, 70, 
                                    ifelse(input$quiz3>0, 40,
                                           ifelse(input$at1>0, 32,
                                                  ifelse(input$quiz2>0, 12,
                                                         ifelse(input$quiz1>0, 6,0
-                                                        ))))))
+                                                        )))))#)
     })
   
   weights <-
@@ -125,46 +125,73 @@ server <- function(input, output) {
         quiz1weight = round(input$quiz1 / 9 * 6, 2),
         quiz2weight = round(input$quiz2 / 9 * 6, 2),
         quiz3weight = round(input$quiz3 / 12 * 8, 2),
-        examweight  = round(input$exam / 100 * 30, 2),
+        #examweight  = round(input$exam / 100 * 30, 2),
         at1weight   = round(input$at1 / 100 * 20, 2),
         at2weight   = round(input$at2 / 100 * 20, 2)
       )
     })
   
-  output$statement <- renderText({
+  weightsum <- reactive({sum(
+    weights()$quiz1weight,
+    weights()$quiz2weight,
+    weights()$quiz3weight,
+    #weights()$examweight,
+    weights()$at1weight,
+    weights()$at2weight
+  )})
+  
+  output$statement1 <- renderText({
     paste(
       "The entered grades amount to ",
-      sum(
-        weights()$quiz1weight,
-        weights()$quiz2weight,
-        weights()$quiz3weight,
-        weights()$examweight,
-        weights()$at1weight,
-        weights()$at2weight
-      ),
+      weightsum(),
       "% out of the ",
       denom(),
-      "% available so far. "
+      "% available so far",
+      ifelse(denom()>0, 
+             paste(", which makes your running average ",
+                   round(weightsum()/denom()*100, 2),
+                   "%.", sep =""),
+             "."), 
+      sep ="" 
     )
   })
-  
-  
-  output$weights <- renderUI({weights()})
-  
-  
+  output$statement2 <- renderText({
+    if((input$target - weightsum())/(100-denom())<1){
+      paste(
+        "To exceed your target of ", 
+        input$target, 
+        "%, you will require an average of over ",
+        round((input$target - weightsum())/(100-denom())*100, 2), 
+        "% across the remaining assessments.")
+    } else {
+      "You cannot reach the specified target with the remaining marks available."
+      }
+  })  
   
   output$targetPlot <- renderPlot({
     
-    # df <- data.frame(Series = factor(c("Current", "Current", "Target", "Target", "Projected", "Projected"), levels = c("Current", "Target", "Projected")),
-    #                  Date = c(input$Start_date, Sys.Date(), input$Start_date, input$Prog_target_date, Sys.Date(), prog()$proj_date),
-    #                  n = c(0, input$Prog_current_n, 0, input$Prog_target_n, input$Prog_current_n, input$Prog_target_n)
-    # )
-    # 
-    # ggplot(df, aes(Date, n, col=Series, linetype=Series)) + geom_line(cex = 1) +
-    #   scale_linetype_manual(values = c("solid", "dotted", "dashed")) + 
-    #   scale_colour_manual(values = c("Black","Blue", "Red")) +
-    #   geom_hline(yintercept = input$Prog_target_n, linetype="dashed") +
-    #   theme_classic() 
+     df <- data.frame(Week = c(0,
+                               4,
+                               7, 
+                               9, 
+                               10, 
+                               11),
+                      Score = c(0,
+                                weights()$quiz1, 
+                                weights()$quiz1 + weights()$quiz2, 
+                                weights()$quiz1 + weights()$quiz2 + weights()$at1,
+                                weights()$quiz1 + weights()$quiz2 + weights()$at1 + weights()$quiz3, 
+                                weights()$quiz1 + weights()$quiz2 + weights()$at1 + weights()$quiz3 + weights()$at2
+                                )
+     )
+     df <- df[1:week(), ]
+     
+     ggplot(df, aes(Week, Score)) +
+       scale_x_continuous(limits = c(0,13), breaks =  1:12, expand = c(0, 0)) +
+       scale_y_continuous(limits = c(0,100), expand = c(0, 0)) +
+       geom_line(cex = 1) +
+       geom_hline(yintercept = input$target, colour = "red", linetype="dashed") +
+       theme_classic()
   })
 }
 
